57196e859f38fa98cf47ba89e583d1f616e210b0
Alexandros Lykesas <alexookah@me.com>
2026-04-15 04:43:02 +0300

    Move demo app to ExampleRouterDemo with full route and view examples

---
+++ ExampleRouterDemo/ExampleRouterDemo/ExampleRouterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleRouterDemoApp.swift
+//  ExampleRouterDemo
+//
+//  Created by Alexandros Lykesas on 15/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleRouterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
