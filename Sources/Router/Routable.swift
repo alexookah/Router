703d7416e6c91a90e5a243d2a37efa79fac187e4
Alexandros Lykesas <alexookah@me.com>
2026-05-01 01:27:06 +0300

    Mark Routable as @MainActor

--- Sources/Router/Routable.swift
+++ Sources/Router/Routable.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+/// A type that maps an enum case to a SwiftUI view, used as the destination type for `Router`.
+@MainActor
 public protocol Routable: Hashable, Identifiable {
     associatedtype ViewType: View
     @ViewBuilder func destination() -> ViewType
